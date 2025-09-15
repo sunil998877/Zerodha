e44f8d3703e5cad1e15b7d2ff8062f41f81c66ee
sunil kumar <your_registered_github_email@example.com>
2025-09-16 01:33:53 +0530

    add signup page

--- frontend/src/landing_page/Signup/SignupPage.jsx
+++ frontend/src/landing_page/Signup/SignupPage.jsx
@@ -1,17 +1,19 @@
 import Trading from "./Trading";
-import Investment from "./Investment";
+// import Investment from "./Investment";
 import Steps from "./Steps";
 import Unbeatable from "./Unbeatable";
+
+// import verifyOTP from "./verifyOTP";
 
 import OpenAccount from '../OpenAccount';
 function Signup() {
     return ( 
         <>
         <Trading/>
-        <Investment/>
+        {/* <Investment/> */}
         <Steps/>
         <Unbeatable/>
-        
+        {/* <verifyOTP/> */}
         
         <OpenAccount/>
       </>
